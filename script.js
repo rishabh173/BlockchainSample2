c9c99843814f6894e8cc9c936560fc8b43fbdabb
rishabh173 <41022485+rishabh173@users.noreply.github.com>
2019-02-20 22:35:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function(){
+	 $('#cover').hover(function() {
+        
+        $('#coverquote').text('A random generic quote on whatever the client wants!');
+        $('#coverdescription').hide();
+        $(this).css('background-image','url(medicine2.jpeg)');
+        
+    }, function() {
+        $('#coverquote').text('Company Name');
+        $('#coverdescription').show();
+        $(this).css('background-image','url(block.jpg)');
+    });
+
+
+
+	
+
+});
